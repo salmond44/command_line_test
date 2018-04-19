ae31f59e07f63ac1ec80a541ffd3afbe81fb7a03
COLLEGEPROWLER\dsalmon <dsalmon@niche.com>
2018-04-19 15:09:21 -0400

    added the bananas you asked for.

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,7 @@
 print('hello world!')
 
 for x in range(5):
-    print('oh no there\'s too much peanut butter!')
+    if (x % 2 == 0):
+        print('oh no there\'s too much peanut butter!')
+    else:
+        print('but not enough bananas')
